e7fb56dd8058fa4a9431dc6a043d549524b0cb1e
Urpi de la Cruz <urpi.delacruz@unmsm.edu.pe>
2020-11-13 00:13:51 -0500

    Corrección de la Configuración

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer')
-    ]
+module.exports = {
+    plugins: [
+       require('tailwindcss'),
+       require('autoprefixer')
+   ]
 }
+   
